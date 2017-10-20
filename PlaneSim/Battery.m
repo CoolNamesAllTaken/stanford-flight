9286c4752667dabdf0b5f6a7f5e56e5a752e0625
John McNelly <jmcnelly@stanford.edu>
2017-10-19 18:36:45 -0700

    added battery object

---
+++ PlaneSim/Battery.m
@@ -0,0 +1,16 @@
+classdef Battery
+	properties
+		voltage % [V] battery voltage (under load, ~1V\cell for NiMh)
+		capacity % [mAh] battery capacity remaining
+	end
+	methods
+		function b = Battery(voltage, capacity)
+			b.voltage - voltage;
+			b.capacity = capacity;
+		end
+
+		function b = discharge(b, battPower, time)
+			b.capacity = b.capacity - (battPower / b.voltage) * time * units.AS_2_MAH;
+		end
+	end
+end
